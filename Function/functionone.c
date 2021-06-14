7704281850ecaf982c93b8695b2f5c9594df331b
mekandemir <merttkandemir@outlook.com>
2021-06-15 02:45:02 +0300

    Two basic functions have been added.

---
+++ Function/functionone.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/*Function that adds the first two numbers of three numbers entered from the keyboard
+and then divides them by the third.*/
+
+
+
+float operation(a,b,c)
+int a,b,c;
+{
+	float g;
+	
+	g=(c/(a+b));
+	
+	return g;
+}
+
+
+int main() {
+	
+	int x,y,z;
+	float result;
+	printf("Enter an integer: ");
+	scanf("%d",&x);
+	printf("Enter an integer : ");
+	scanf("%d",&y);
+	printf("Enter an integer: : ");
+	scanf("%d",&z);
+	
+	result=islem(x,y,z);
+	printf("Result : ",result);
+
+	
+	return 0;
+}
---
+++ Function/functiontwo.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Function that returns the integer part of the difference of two float*/
+
+int main() {
+	
+	int intsubtraction(a,b)
+	float a,b;
+	{
+		int g;
+		g=a-b;
+		return g;
+	}
+	
+	float x,y;
+	int result;
+	x=10.23412;
+	y=54.2131241;
+	result=intsubtraction(x,y);
+	
+	
+	
+	printf("%d",result);
+
+	
+	
+	return 0;
+}
